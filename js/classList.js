dae25eaf77fb7913dd748f7bfc8867d5543418a0
Maria Betances <mbetances1002@gmail.com>
2018-07-02 22:03:09 -0400

    code cleanup

--- js/classList.js
+++ js/classList.js
@@ -1,21 +1,21 @@
 const classes = [{
-    name: "Class 1",
-    studentIds: [1, 2]
+  name: 'Class 1',
+  studentIds: [1, 2]
 }, {
-    name: "Class 2",
-    studentIds: []
+  name: 'Class 2',
+  studentIds: []
 }, {
-    name: "Class 3",
-    studentIds: []
+  name: 'Class 3',
+  studentIds: []
 }];
 
 // sort classes array in descending order
 classes.sort(function compare (a, b) {
   let classA = a.name.toLowerCase();
   let classB = b.name.toLowerCase();
-  
+
   let comparison = 0;
-  
+
   if (classA > classB) {
     comparison = -1;
   } else if (classA < classB) {
@@ -34,31 +34,31 @@ container.appendChild(classList);
 app.appendChild(container);
 
 // loop through the classes array and create and append the necessary DOM elements
-classes.map(function(singleClass) {
-    let classElement = document.createElement('li');
-    let classTitle = document.createElement('span');
-    let image = document.createElement('img');
-    let classCount = document.createElement('span');
-    let classButton = document.createElement('button');
-    let buttonText = document.createTextNode('Begin');
-    let classTitleText = document.createTextNode(singleClass.name);
-    let classSummary = document.createTextNode(`${singleClass.studentIds.length} Students`);
+classes.map(function (singleClass) {
+  let classElement = document.createElement('li');
+  let classTitle = document.createElement('span');
+  let image = document.createElement('img');
+  let classCount = document.createElement('span');
+  let classButton = document.createElement('button');
+  let buttonText = document.createTextNode('Begin');
+  let classTitleText = document.createTextNode(singleClass.name);
+  let classSummary = document.createTextNode(`${singleClass.studentIds.length} Students`);
 
-    classElement.setAttribute('class', 'class-element');
-    image.setAttribute('class', 'class-image');
-    image.setAttribute('alt', `${singleClass.name.toLowerCase()} image`)
-    classTitle.setAttribute('class', 'class-title');
-    classCount.setAttribute('class', 'class-count');
-    image.setAttribute('src',
-      'https://s3-us-west-2.amazonaws.com/vidcode/assets/coursethumbs/creative-coding.png');
-    classButton.setAttribute('class', 'class-button');
-    
-    classButton.appendChild(buttonText);
-    classCount.appendChild(classSummary);
-    classTitle.appendChild(classTitleText);
-    classElement.appendChild(image);
-    classElement.appendChild(classTitle);
-    classElement.appendChild(classCount);
-    classElement.appendChild(classButton);
-    classList.appendChild(classElement);
+  classElement.setAttribute('class', 'class-element');
+  image.setAttribute('class', 'class-image');
+  image.setAttribute('alt', `${singleClass.name.toLowerCase()} image`)
+  classTitle.setAttribute('class', 'class-title');
+  classCount.setAttribute('class', 'class-count');
+  image.setAttribute('src',
+    'https://s3-us-west-2.amazonaws.com/vidcode/assets/coursethumbs/creative-coding.png');
+  classButton.setAttribute('class', 'class-button');
+
+  classButton.appendChild(buttonText);
+  classCount.appendChild(classSummary);
+  classTitle.appendChild(classTitleText);
+  classElement.appendChild(image);
+  classElement.appendChild(classTitle);
+  classElement.appendChild(classCount);
+  classElement.appendChild(classButton);
+  classList.appendChild(classElement);
 });
